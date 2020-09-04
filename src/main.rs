e09b598c38195ad04f6d14c2b83f7560b54cdbeb
Christian Kuhl <christian.kuhl84@gmail.com>
2020-09-04 10:21:09 +0200

    Binary heap based wheel implementation

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,45 @@ use std::time::SystemTime;
 use std::collections::BinaryHeap;
 use std::cmp::Reverse;
 
+const WHEEL_SIZE: usize = 48;
+const WHEEL: [usize; WHEEL_SIZE] = [2, 4, 2, 4, 6, 2, 6, 4, 2, 4, 6, 6, 2, 6, 4, 2, 6, 4, 6, 8, 4, 2, 4, 2, 4, 8, 6, 4, 6, 2, 4, 6, 2, 6, 6, 4, 2, 4, 6, 2, 6, 4, 2, 4, 2, 10, 2, 10]; 
+const SMALL_PRIMES: [usize; 5] = [2, 3, 5, 7, 11];
+
+struct Current {
+    value: usize,
+    wheel_index: usize,
+}
+
 struct Primes {
-    current: usize,
+    current: Current,
     limit: Option<usize>,
     count: Option<usize>,
-    composites: BinaryHeap<Reverse<[usize; 2]>>,
-    yielded: usize
+    composites: BinaryHeap<Reverse<[usize; 3]>>,
+    yielded: usize,
+    small_primes: Box<dyn Iterator<Item=&'static usize>>,
 }
 
 impl Primes {
-    fn up_to(limit: usize) -> Primes {
-        Primes { current: 2, yielded: 0, count: Some(limit), limit: None, composites: BinaryHeap::new() }
-    }
     fn all() -> Primes {
-        Primes { current: 2, yielded: 0, count: None, limit: None, composites: BinaryHeap::new() }
+        let small_primes = SMALL_PRIMES.iter();
+        Primes { 
+            current: Current { value: 2, wheel_index: 0 }, 
+            yielded: 0, 
+            count: None, 
+            limit: None, 
+            composites: BinaryHeap::new(), 
+            small_primes: Box::new(small_primes)
+        }
+    }
+    fn up_to(limit: usize) -> Primes {
+        let mut p = Primes::all();
+        p.count = Some(limit);
+        return p
     }
     fn less_than(limit: usize) -> Primes {
-        Primes { current: 2, yielded: 0, count: None, limit: Some(limit), composites: BinaryHeap::new() }
+        let mut p = Primes::all();
+        p.limit = Some(limit);
+        return p
     }
 }
 
@@ -26,34 +48,34 @@ impl Iterator for Primes {
     type Item = usize;
     fn next(&mut self) -> Option<usize> {
         if let Some(limit) = self.limit {
-            if limit < self.current { return None }
+            if limit < self.current.value { return None }
         }
         if let Some(limit) = self.count {
-            if limit < self.yielded { return None }
-        }
-        if self.current == 2 {
-            self.current += 1; 
-            return Some(2) 
+            if limit <= self.yielded { return None }
         }
         let mut is_prime = true;
+        if let Some(prime) = self.small_primes.next() {
+            self.current.value = *prime;
+        } else {
+            self.current.value += WHEEL[self.current.wheel_index];
+            self.current.wheel_index = ( self.current.wheel_index + 1 ) % WHEEL_SIZE;
+        }
         while let Some(mut head) = self.composites.peek_mut() {
-            let Reverse([composite, prime]) = *head;
-            if composite > self.current {
+            let Reverse([composite, prime, old_wheel]) = *head;
+            if composite > self.current.value {
                 break;
             } else {
                 is_prime = false;
-                *head = Reverse([composite + 2 * prime, prime]);
+                *head = Reverse([composite + WHEEL[old_wheel % 48] * prime, prime, (old_wheel + 1) % WHEEL_SIZE]);
             }
         } 
         if is_prime {
-            self.composites.push(Reverse([self.current.pow(2), self.current]));
-            self.current += 2;
-            // println!("{}: {:?}", self.current, self.composites);
+            if self.current.value > 7 {
+                self.composites.push(Reverse([self.current.value.pow(2), self.current.value, self.current.wheel_index]));
+            }
             self.yielded += 1;
-            return Some(self.current - 2)
+            return Some(self.current.value)
         } else { 
-            self.current += 2;
-            // println!("{}: {:?}", self.current, self.composites);
             return self.next()
          }
     }
@@ -64,7 +86,6 @@ fn main() {
     for (j, p) in Primes::all().enumerate() {
         if j % 1_000_000 == 999_999 {
             let elapsed = now.elapsed().unwrap().as_millis();
-            // println!("{}: {}", j+1, p);
             println!("{} millionth prime is {}, time elapsed: {} ms", (j + 1) / 1_000_000 as usize, p, elapsed);
             now = SystemTime::now();
         }
